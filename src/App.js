bc197b1655e7678d51e113540510547448ece6ba
ohuji <juhsalomaki@gmail.com>
2020-05-11 13:41:21 +0300

    Alphabet, Amazon and Tesla pages and deletion of couple of buttons

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import './App.css';
 import Navbar from "./components/Navbar";
 import { BrowserRouter, Route, Link } from "react-router-dom";
 import Footer from "./components/Footer"
-import Main from "./components/Main"
 import Login from "./components/Login"
 import Register from "./components/Register"
 import Home from "./components/Home";
@@ -28,39 +27,44 @@ import Waltdisney from './components/Waltdisney';
 import Oracle from './components/Oracle';
 import Mcdonalds from './components/Mcdonalds';
 import Bankofamerica from './components/Bankofamerica';
+import Amazon from './components/Amazon';
+import Tesla from './components/Tesla';
+import Alphabet from './components/Alphabet';
 
 class App extends Component {
  
 
   render() {
-  return (
-   <div className="App">  
-    <BrowserRouter>
-     <Route exact path="/" component={Main}/>
-     <Route path="/login" component={Login}/>
-     <Route path="/register" component={Register}/>
-     <Route path="/Home" component={Home}/>
-     <Route path="/AAPL" component={Apple}/>
-     <Route path="/NYSE" component={Nyse}/>
-     <Route path="/Nasdaq" component={Nasdaq}/>
-     <Route path="/Currencies" component={Currencies}/>
-     <Route path="/Sse" component={Sse}/>
-     <Route path="/LSE" component={Lse}/>
-     <Route path="/ETF" component={Etf}/>
-     <Route path="/Crypto" component={Crypto}/>
-     <Route path="/ASX" component={Asx}/>
-     <Route path="/BRK.A" component={BerkshireHathaway}/>
-     <Route path="/BABA" component={Alibaba}/>
-     <Route path="/JPM" component={Jpmorganchase}/>
-     <Route path="/V" component={Visa}/>
-     <Route path="/XOM" component={Exxonmobil}/>
-     <Route path="/KO" component={CocaCola}/>
-     <Route path="/WMT" component={Walmart}/>
-     <Route path="/DIS" component={Waltdisney}/>
-     <Route path="/ORCL" component={Oracle}/>
-     <Route path="/MCD" component={Mcdonalds}/>
-     <Route path="/BAC" component={Bankofamerica}/>
-    </BrowserRouter>
+   return (
+    <div className="App">  
+     <BrowserRouter>
+      <Route exact path="/" component={Home}/>
+      <Route path="/login" component={Login}/>
+      <Route path="/register" component={Register}/>
+      <Route path="/AAPL" component={Apple}/>
+      <Route path="/NYSE" component={Nyse}/>
+      <Route path="/Nasdaq" component={Nasdaq}/>
+      <Route path="/Currencies" component={Currencies}/>
+      <Route path="/Sse" component={Sse}/>
+      <Route path="/LSE" component={Lse}/>
+      <Route path="/ETF" component={Etf}/>
+      <Route path="/Crypto" component={Crypto}/>
+      <Route path="/ASX" component={Asx}/>
+      <Route path="/BRK.A" component={BerkshireHathaway}/>
+      <Route path="/BABA" component={Alibaba}/>
+      <Route path="/JPM" component={Jpmorganchase}/>
+      <Route path="/V" component={Visa}/>
+      <Route path="/XOM" component={Exxonmobil}/>
+      <Route path="/KO" component={CocaCola}/>
+      <Route path="/WMT" component={Walmart}/>
+      <Route path="/DIS" component={Waltdisney}/>
+      <Route path="/ORCL" component={Oracle}/>
+      <Route path="/MCD" component={Mcdonalds}/>
+      <Route path="/BAC" component={Bankofamerica}/>
+      <Route path="/AMZN" component={Amazon}/>
+      <Route path="/TSLA" component={Tesla}/>
+      <Route path="/GOOGL" component={Alphabet}/>
+     </BrowserRouter>
    </div>
   )
   }
--- src/components/Navbartwo.js
+++ src/components/Navbartwo.js
@@ -7,28 +7,9 @@ const Navbartwo = () => {
     <nav className="navbar">
 
         <div className="navbar-brand offset-2">
-         <Link to="/home" className="text-white">
+         <Link to="/" className="text-white">
                Logo
          </Link>
-       </div>
-
-         <div className="row">
-            <button className="btn nav-item ">
-               <Link
-                to="/login"
-                className="PercentageNavText" >
-                  Profiili
-               </Link>
-            </button>
-                  
-            <button className="btn nav-item ">
-               <Link
-                to="/"
-                className="PercentageNavText">
-                  Kirjaudu ulos
-               </Link>
-            </button>
-
        </div>
     </nav>
    )
